c60e4b64ee684e25a24e315db914fe709bbbe0cc
Wangabai <1059917258@qq.com>
2021-03-27 17:34:59 +0800

    complete detail

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -248,18 +248,20 @@ export default {
           this.phoneList = [res.list.slice(0, 4), res.list.slice(4, 8)]
         })
     },
-    addCart() {
+    addCart(id) {
       this.showModal = true
-      //
-      // this.$http
-      //   .post('/carts', {
-      //     productId: id,
-      //     selected: true,
-      //   })
-      //   .then(() => {})
-      //   .catch(() => {
-      //     this.showModal = true
-      //   })
+      this.$http
+        .post('/carts', {
+          productId: id,
+          selected: true,
+        })
+        .then((res) => {
+          this.showModal = true
+          this.$store.dispatch('saveCartCount', res.cartTotalQuantity)
+        })
+        .catch(() => {
+          this.showModal = true
+        })
     },
     goToCart() {
       this.$router.push('/cart')
